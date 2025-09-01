eb8dbfaaae3ec69fcc032a1b0aab18dff302d813
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-01 19:00:27 +0530

    intial commit

---
+++ duplicate.java
@@ -0,0 +1,18 @@
+// print duplicate values in java 
+
+public class duplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 2, 3, 1};
+        System.out.println("duplicate values to print");
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ java_fundamentals/java1.java
@@ -0,0 +1,22 @@
+
+public class java1 {
+
+    public static void main(String[] args) {
+        byte b = 20;
+        short s = 30;
+        int i = 10;
+        float f = 15;
+        double d = 20;
+        char c = 'I';
+        boolean bool = true;
+
+        System.out.println("byte:" + b);
+        System.out.println("short:" + s);
+        System.out.println("int:" + i);
+        System.out.println("float:" + f);
+        System.out.println("double:" + d);
+        System.out.println("char:" + c);
+        System.out.println("boolean:" + bool);
+
+    }
+}
