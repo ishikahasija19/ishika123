bfc2dd694b2af3ba80fcf4f27cb5a6b03128000f
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:29:12 +0530

    java8.java

---
+++ java_fundamentals/java8.java
@@ -0,0 +1,12 @@
+public class java8{
+    public static void main(String[]args){
+        int i=10;
+        double d=i;
+
+       
+        double d1=7;
+        int i1=(int)d1;
+        System.out.println("Implicit:"+d);
+        System.out.println("Explicit:"+i1);
+    }
+}
