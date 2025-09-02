aa3d96fd8440e5490e095ef0e29ba5ee6377392e
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:28:43 +0530

    java7.java

---
+++ java_fundamentals/java7.java
@@ -0,0 +1,20 @@
+public class java7{
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+    public static void main(String[] args) {
+        java7 obj=new java7();
+        System.out.println("byte:"+obj.b);
+        System.out.println("short:"+obj.s);
+        System.out.println("int:"+obj.i);
+        System.out.println("long:"+obj.l);
+        System.out.println("float:"+obj.f);
+        System.out.println("double:"+obj.d);
+        System.out.println("char:"+obj.c);
+        System.out.println("boolean"+obj.bool);
+        
