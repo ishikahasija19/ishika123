26d8068c1044a7118e2003f253a03d156a2eb2c7
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:23:17 +0530

    java15.java

---
+++ java_fundamentals/java15.java
@@ -0,0 +1,8 @@
+public class java15{
+    public static void main(String[] args) {
+        int a =10;
+        Integer b = new Integer(10);
+        System.out.println("primitive int a ="+a);
+        System.out.println("Reference Integer b="+b);
+    }
+}
