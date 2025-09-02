f45fd850796b2f9dac8285026e1f282c49d55e97
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:23:45 +0530

    java16.java

---
+++ java_fundamentals/java16.java
@@ -0,0 +1,9 @@
+public class java16{
+    static final double PI=3.14;
+    public static void main(String[] args) {
+        double radius =5;
+        double area=PI*radius*radius;
+        System.out.println("area of circle:"+area);
+        
+    }
+}
