19169b48cc35e772852ccb6ac3d034693f5611a6
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:24:13 +0530

    java17.java

---
+++ java_fundamentals/java17.java
@@ -0,0 +1,10 @@
+public class java17{
+    public static void main(String[] args) {
+        var number=100;
+        var name="java";
+        var pi=3.14;
+        System.out.println("number:"+number);
+        System.out.println("name:"+name);
+        System.out.println("PI:"+pi);
+    }
+}
