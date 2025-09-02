d7810d5c3f78fb7a73d8d9e7ba84c1946eeca651
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:22:33 +0530

    java10.java

---
+++ java_fundamentals/java10.java
@@ -0,0 +1,8 @@
+public class java10{
+    public static void main(String[] args) {
+        final double PI=3.14;
+        double radius =5.0;
+        double area= PI*radius*radius;
+        System.out.println("area:"+area);
+    }
+}
