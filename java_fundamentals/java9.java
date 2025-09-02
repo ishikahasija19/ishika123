328fd807a4cf9f3e566450c5abbbd8145d8339aa
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:29:40 +0530

    java9.java

---
+++ java_fundamentals/java9.java
@@ -0,0 +1,10 @@
+public class java9{
+    public static void main(String[]args){
+        int i=10;
+        Integer obj =i;
+
+        int j = obj;
+        System.out.println("Autoboxing:"+obj);
+        System.out.println("unboxing:"+j);
+    }
+}
