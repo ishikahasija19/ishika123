a2c99348a6713461754e55292131c8b209e8442c
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:28:13 +0530

    java6.java

---
+++ java_fundamentals/java6.java
@@ -0,0 +1,11 @@
+public class java6{
+    public static void main(String[] args) {
+        int num=0;
+        if(num>0)
+        System.out.println("positive");
+        else if(num<0)
+        System.out.println("Negative");
+     else
+      System.out.println("zero");
+    }
+}
