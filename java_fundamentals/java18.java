40830fd3ce081adc0d498987ef14212f694a170b
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:24:41 +0530

    java18.java

---
+++ java_fundamentals/java18.java
@@ -0,0 +1,9 @@
+public class java18{
+    public static void main(String[] args) {
+        int max=Integer.MAX_VALUE;
+        System.out.println("MAX int:"+max);
+        int overflow =max+1;
+
+        System.out.println("after overflow:"+ overflow);
+    }
+}
