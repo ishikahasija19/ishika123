e361f21ad69580421bf616363a6946c16d5fa687
ishikahasija19 <ishikahasija00@gmail.com>
2025-09-02 09:21:55 +0530

    java14.java

---
+++ java_fundamentals/java14.java
@@ -0,0 +1,11 @@
+public class java14{
+    public static void main(String[] args) {
+        int number =50;
+        String binary=Integer.toBinaryString(number);
+        String hex =Integer.toHexString(number);
+        System.out.println("Number:"+number);
+        System.out.println("binary:"+ binary);
+        System.out.println("hexadecimal:"+ hex);
+
+    }
+}
